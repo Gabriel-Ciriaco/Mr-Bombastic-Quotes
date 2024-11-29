848340f39bac273afd844ab123b27a2086ec9481
Gasegamer <gabrielciricarvalho@gmail.com>
2024-11-28 22:32:04 -0300

    Novos testes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let canvas = document.getElementById("quote-canvas");
+let ctx = canvas.getContext("2d");
+
 let sentence = document.getElementById("sentence");
 
 let quotes = QUOTES;
@@ -15,4 +18,31 @@ if (!/[.!?]$/.test(selected_quote)) {
   selected_quote += '.';
 }
 
-sentence.textContent = `"${selected_quote}"`;
+selected_quote = `"${selected_quote}"`;
+
+let image = new Image();
+image.src = "./mr-bombastic.png";
+
+image.onload = function () {
+  const canvasWidth = 600;
+  const canvasHeight = 400;
+
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
+  ctx.fillStyle = "#34495e";
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  ctx.fillStyle = "#ecf0f1";
+  ctx.font = "24px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText(selected_quote, canvasWidth / 2, 100);
+
+  const imgX = (canvasWidth - 200) / 2;
+  const imgY = 100;
+  ctx.drawImage(image, imgX, imgY, 200, 200);
+
+  // Draw the "said by" text
+  ctx.font = "italic 18px Arial";
+  ctx.fillText("~ said by Mr. Bombastic", canvasWidth / 2, imgY + 200);
+};
