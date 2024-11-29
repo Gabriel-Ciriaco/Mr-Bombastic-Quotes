ae5f23db5fd6c223bc3c8af16f03650b57213b8b
Gasegamer <gabrielciricarvalho@gmail.com>
2024-11-28 23:07:26 -0300

    Testando Vercel

---
+++ api/generate-quotes.js
@@ -0,0 +1,32 @@
+import { createCanvas, loadImage } from "@napi-rs/canvas";
+import quotes from "./quotes.json";
+
+export default async function handler(req, res) {
+  let day_quote = Math.floor(Math.random() * quotes.length);
+
+  const randomQuote = quotes[day_quote];
+
+  const canvas = createCanvas(600, 400);
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#34495e";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#ecf0f1";
+  ctx.font = "24px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText(`"${randomQuote}"`, canvas.width / 2, 100);
+
+  const image = await loadImage(
+    "https://raw.githubusercontent.com/Gabriel-Ciriaco/Mr-Bombastic-Quotes/refs/heads/main/public/mr-bombastic.png"
+  );
+
+  ctx.drawImage(image, (canvas.width - 200) / 2, 100, 200, 200);
+
+  ctx.font = "italic 18px Arial";
+  ctx.fillText("~ said by Mr. Bombastic", canvas.width / 2, 320);
+
+  const base64Image = canvas.toDataURL("image/png");
+
+  res.status(200).json({ imageUrl: base64Image });
+}
