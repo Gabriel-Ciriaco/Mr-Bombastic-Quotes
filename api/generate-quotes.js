c16416c749da4e34d151d5147f2d5b61e11ea06f
Gabriel Ciriaco de Carvalho <66225865+Gabriel-Ciriaco@users.noreply.github.com>
2024-11-29 01:22:38 -0300

    Raw file

--- api/generate-quotes.js
+++ api/generate-quotes.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   GlobalFonts.register(Buffer.from(font_buffer), "sans-serif")
 
   const response = await fetch(
-    "https://raw.githubusercontent.com/Gabriel-Ciriaco/Mr-Bombastic-Quotes/main/api/quotes.json");
+    "https://raw.githubusercontent.com/Gabriel-Ciriaco/Mr-Bombastic-Quotes/refs/heads/main/api/quotes.json");
 
   const quotes = await response.json();
 
